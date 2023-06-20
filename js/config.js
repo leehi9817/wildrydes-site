6462dfc9a0f567ebed4ebf83140054d92160065c
leehi <gitlab-test@example.com>
2023-06-20 10:20:38 +0900

    eidt_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-2_yLQ8UrBhf', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '47ie1aasjugoghscqmvd1hga7c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-northeast-2_sVFJDJcyc', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7djlbudtqv71898telu810dfun', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
